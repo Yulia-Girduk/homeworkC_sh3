55f481ab4f8b417a413cabaa00caef0c7ba3135b
Nick Gir <mbiko@ya.ru>
2023-03-03 15:29:03 +0300

    add task23

---
+++ task23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+double GetDegree (double numberDegree, double numDegree) // метод возвращает число, возведенное в заданную степень
+                                                         // numberDegree - число, которое нужно возвести в степень (6)
+                                                         // numDegree - число степени (2)   
+                                                         // (6 во 2 степени = 36)
+{
+    double resultDegree = Math.Pow(numberDegree, numDegree);
+    return resultDegree;
+}
+
+Console.Write("Введите число N: ");
+double N = double.Parse(Console.ReadLine());
+
+Console.Write($"Таблица кубов до {N} -> ");
+for (int i = 1; i <= N; i++)
+{
+  double result = GetDegree(i, 3);
+  Console.Write($" {result}  ");
+}
